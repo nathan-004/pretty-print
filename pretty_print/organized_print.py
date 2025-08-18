c715321f0efb84923a8bfab8b8f0fb2435974759
Nathan <n49874687@gmail.com>
2025-08-18 18:37:31 +0200

    custom fonctions `__setitem__`

--- pretty_print/organized_print.py
+++ pretty_print/organized_print.py
@@ -10,12 +10,30 @@ class Cell:
     def __repr__(self):
         return f"{self.content}"
 
+class Row(list):
+    """Class representing a row in the organized print table."""
+    
+    def __init__(self, cells: list[Cell]):
+        super().__init__(cells)
+    
+    def __repr__(self):
+        return " | ".join(str(cell) for cell in self)
+    
+    def __setitem__(self, index, value):
+        """Set a cell at a specific index."""
+        if isinstance(value, Cell):
+            super().__setitem__(index, value)
+        elif isinstance(value, str):
+            super().__setitem__(index, Cell(value))
+        else:
+            raise ValueError("Value must be an instance of Cell or a string")
+
 class OrganizedPrint(list):
     """Class for organized printing in the terminal."""
 
     def __init__(self, cell_width: int = 1, cell_height: int = 1):
         """Initialize the OrganizedPrint with the number of divisions."""
-        table = [[Cell() for _ in range(cell_width)] for _ in range(cell_height)]
+        table = [Row([Cell() for _ in range(cell_width)]) for _ in range(cell_height)]
         self.cell_width = cell_width
         self.cell_height = cell_height
         super().__init__(table)
@@ -39,6 +57,17 @@ class OrganizedPrint(list):
 
         return "\n".join(output)
     
+    def __setitem__(self, index, value):
+        """Set a row at a specific index."""
+        if isinstance(value, Row):
+            super().__setitem__(index, value)
+        elif isinstance(value, str):
+            super().__setitem__(index, Row([Cell(value) for _ in range(self.cell_width)]))
+        elif isinstance(value, list):
+            super().__setitem__(index, Row([Cell(content) for content in value]))
+        else:
+            raise ValueError("Value must be an instance of Row")
+    
     def get_max_length_colums(self, column_index: int) -> int:
         """Get the maximum element length in a specific column."""
         maximum_length = 0
@@ -52,6 +81,6 @@ class OrganizedPrint(list):
 def main():
     """Main function to demonstrate organized printing."""
     op = OrganizedPrint(4, 5)
-    op.add_content(0, 0, "Header 1")
-    op.add_content(0, 1, "Header 2")
+    op[0] = ["Header 1", "Header 2", "Header 3", "Header 4"]
+    op[1][0] = "Test"
     print(op)
